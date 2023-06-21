7b1a071f25919ca5a0278bf04fe41f976dc78fe6
tanndlin <tanndlin@gmail.com>
2023-06-21 13:23:56 -0400

    Remove useless div

--- src/components/Options/IOItemViewer.tsx
+++ src/components/Options/IOItemViewer.tsx
@@ -15,35 +15,33 @@ const IOItemViewer = (props: Props) => {
     const { item, amount } = ioItem;
 
     return (
-        <li>
-            <div className="flex py-2 relative">
-                <CancelButton
-                    onClick={() => {
+        <li className="flex py-2 relative">
+            <CancelButton
+                onClick={() => {
+                    const newItems = [...ioItems];
+                    newItems.splice(index, 1);
+                    setIOItems(newItems);
+                }}
+            />
+
+            <ItemImage item={item} />
+            <div className="flex flex-col my-auto ml-2">
+                <span className="text-xl">{item.name}</span>
+                <input
+                    type="number"
+                    value={amount}
+                    className="px-2"
+                    onChange={(e) => {
+                        e.target.value = Math.max(
+                            0,
+                            +e.target.value
+                        ).toString();
+
                         const newItems = [...ioItems];
-                        newItems.splice(index, 1);
+                        newItems[index].amount = +e.target.value;
                         setIOItems(newItems);
                     }}
                 />
-
-                <ItemImage item={item} />
-                <div className="flex flex-col my-auto ml-2">
-                    <span className="text-xl">{item.name}</span>
-                    <input
-                        type="number"
-                        value={amount}
-                        className="px-2"
-                        onChange={(e) => {
-                            e.target.value = Math.max(
-                                0,
-                                +e.target.value
-                            ).toString();
-
-                            const newItems = [...ioItems];
-                            newItems[index].amount = +e.target.value;
-                            setIOItems(newItems);
-                        }}
-                    />
-                </div>
             </div>
         </li>
     );
