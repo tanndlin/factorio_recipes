363e4f1c34974bd98760b0e47d0eba2d3c86e305
tanndlin <tanndlin@gmail.com>
2023-06-21 13:23:52 -0400

    Tabulate header

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
 const Header = () => {
+    const currentPage = window.location.pathname;
+
     return (
         <div className="header flex border-b-[1px] border-gray-500 text-xl px-8 flex-initial w-full">
-            <ul className="flex gap-10 py-4">
-                <li>
+            <ul className="flex gap-10">
+                <li
+                    className={
+                        'tab' + (currentPage === '/' ? ' active-tab' : '')
+                    }
+                >
                     <a href="/">Home</a>
                 </li>
-                <li>
+                <li
+                    className={
+                        'tab' +
+                        (currentPage === '/recipes' ? ' active-tab' : '')
+                    }
+                >
                     <a href="/recipes">Recipes</a>
                 </li>
             </ul>
