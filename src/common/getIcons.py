24ab74d2d1cde9edc38da5d1ec7ca087b29e75cc
tanndlin <tanndlin@gmail.com>
2023-06-21 13:23:51 -0400

    Finish updating pics

--- src/common/getIcons.py
+++ src/common/getIcons.py
@@ -9,16 +9,19 @@ def getIcon(url):
     print(url)
     os.system(f'curl -o {outputPath}/{url.split("/")[-1]} {url}')
 
-def createUrl(id):
-    replaced = id.replace('-', '_').capitalize()
-    replaced = replaced.replace('mk1', 'MK1').replace('mk2', 'MK2')
+def createUrl(name):
+    if name == 'Rail':
+        replaced = 'Straight_rail'
+    else:
+        replaced = name.replace(' ', '_').capitalize()
+        replaced = replaced.replace('mk1', 'MK1').replace('mk2', 'MK2')
     return f'https://wiki.factorio.com/images/thumb/{replaced}.png/48px-{replaced}.png'
 
 if __name__ == '__main__':
     file = '../assets/recipes.json'
     with open(file, 'r') as f:
         data = json.load(f)
-    
+
         with multiprocessing.Pool(16) as p:
-            p.map(getIcon, [createUrl(recipe['id']) for recipe in data])
+            p.map(getIcon, [createUrl(recipe['name']) for recipe in data])
 
