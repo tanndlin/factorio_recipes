e97342415bc256a03b53f0668cecd2137ac92a72
tanndlin <tanndlin@gmail.com>
2023-06-21 13:23:51 -0400

    Fix some icons, TODO

---
+++ src/common/getIcons.py
@@ -0,0 +1,24 @@
+import json
+import multiprocessing
+import os
+
+
+outputPath = '../../public/images'
+
+def getIcon(url):
+    print(url)
+    os.system(f'curl -o {outputPath}/{url.split("/")[-1]} {url}')
+
+def createUrl(id):
+    replaced = id.replace('-', '_').capitalize()
+    replaced = replaced.replace('mk1', 'MK1').replace('mk2', 'MK2')
+    return f'https://wiki.factorio.com/images/thumb/{replaced}.png/48px-{replaced}.png'
+
+if __name__ == '__main__':
+    file = '../assets/recipes.json'
+    with open(file, 'r') as f:
+        data = json.load(f)
+    
+        with multiprocessing.Pool(16) as p:
+            p.map(getIcon, [createUrl(recipe['id']) for recipe in data])
+
